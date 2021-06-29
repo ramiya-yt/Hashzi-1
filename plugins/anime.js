fe9c915eac57a04a7a8c5bf89bb948864868b4ff
TeenuhX <84779528+tenuh@users.noreply.github.com>
2021-06-29 15:10:22 +0530

    Update anime.js
    new command 🙂

--- plugins/anime.js
+++ plugins/anime.js
@@ -97,7 +97,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by WhatsAsenaPublic'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
 
     }));
 }
@@ -191,7 +191,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by WhatsAsenaPublic'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
 
     }));
 }
