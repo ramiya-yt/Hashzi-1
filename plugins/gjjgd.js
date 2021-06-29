9adc8e331d9822cba287716c5d0d2cee0c535461
TeenuhX <84779528+tenuh@users.noreply.github.com>
2021-06-29 18:50:30 +0530

    Create gjjgd.js

---
+++ plugins/gjjgd.js
@@ -0,0 +1,13 @@
+/*Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'teenuh1', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("*🎭ɴᴇᴏᴛʀᴏ𝙭🪐* *ᴀʟɪᴠᴇ ɴᴏᴡ*");
+
+}));
