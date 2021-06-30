949b55d8484079735ec9be3dc3b6bd7a97f6e4a1
TeenuhX <84779528+tenuh@users.noreply.github.com>
2021-06-30 11:05:29 +0530

    Create prologos.js

---
+++ plugins/prologos.js
@@ -0,0 +1,12 @@
+/*Codded by @phaticusthiccy
+Telegram: t.me/phaticusthiccy
+Instagram: www.instagram.com/kyrie.baran
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'logopro', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("*╔═════⦁⦁⦁⦁👽⦁⦁⦁⦁═════╗*\n*💠▬𝗟𝗼𝗴𝗼 𝗣𝗮𝗰𝗸⚡▬💠*\n     *🛸𝗧𝗲𝗲𝗻𝘂𝗵𝗣𝗿𝗼🪐.*\n\n🎯 Command: *.grass*\n*⚡Description: *It Converts Text with Grass Background*\n\n🎯 Command: *.fleaves*\n*⚡Description: *It Converts Text with fallleaves Background*\n\n🎯 Command: *.avte*\n*⚡Description: *It Converts Text to Avatar Logo*\n\n*🎯 Command: *.legend*\n*⚡Description: *It Converts Text to Mobile Legend logo\n\n🎯 Command: *.cartoon*\n⚡Description: *It Converts Text to cartoon*\n\n🎯 Command: *.anony*\n⚡Description: *It Converts Text to Anonymous Neon*\n\n🎯 Command: *.bwall*\n⚡Description: *It Converts Text to breakwall*\n*𝗧𝗲𝗲𝗻𝘂𝗵𝗣𝗿𝗼");
+}));
