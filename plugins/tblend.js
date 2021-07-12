2cc01292a036d7c13ce5afd9f545a277836cb434
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-07-13 03:05:12 +0530

    Update tblend.js

--- plugins/tblend.js
+++ plugins/tblend.js
@@ -16,7 +16,7 @@ const Lang = Language.getString('conventer');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'tblend ?(.*)', fromMe: true, desc: Lang.T_DESC}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'nvideo ?(.*)', fromMe: true, desc: Lang.T_DESC}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
 
