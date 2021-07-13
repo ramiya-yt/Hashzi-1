ee54f1aa1661cdfaca571889aa85f919d4f0248a
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-07-13 21:07:06 +0530

    Update neotromem.js

--- plugins/neotromem.js
+++ plugins/neotromem.js
@@ -12,7 +12,7 @@ const Ln = "*▷🎭ɴᴇᴏᴛʀᴏ-𝙭🪐මීම් ලැයිස්ත�
 
 if (Config.WORKTYPE == 'public') {
     
-      Asena.addCommand({pattern: 'mmpack', fromMe: false, desc: Ln,}, (async (message, match) => {
+      Asena.addCommand({pattern: 'nop45', fromMe: false, desc: Ln,}, (async (message, match) => {
 
     await message.client.sendMessage(
   
