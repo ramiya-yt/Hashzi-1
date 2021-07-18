51390108069cd2ae7f47235f1a6ee75afafd7f45
Neotro23 <87609444+Neotro23@users.noreply.github.com>
2021-07-19 05:20:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -45,6 +45,7 @@ module.exports = {
     BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
     AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
+    MENUTEXT: process.env.MENU === undefined ?'*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*' : process.env.MENU,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
     BRANCH: 'master',
     HEROKU: {
