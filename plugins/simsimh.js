4976aff3f1bf6a62a71f5b49413cecf2144804e6
Neotro23 <87609444+Neotro23@users.noreply.github.com>
2021-07-20 23:14:56 +0530

    Create simsimh.js

--- plugins/simsimh.js
+++ plugins/simsimh.js
@@ -28,7 +28,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+    Asena.addCommand({pattern: 'bot ?(.*)', fromMe: '+Config.AIBOT+', desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
