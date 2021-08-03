f4dadf911fa5e79586235d8681fd4d6d33f72bae
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-08-03 16:35:06 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2,2123,8]
+    conn.version = [2, 2123, 8]
     setInterval(async () => { 
         var getGMTh = new Date().getHours()
         var getGMTm = new Date().getMinutes()
