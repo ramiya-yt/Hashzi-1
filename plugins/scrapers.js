6981404882312402f08d58378f8a69e252e04215
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-08-08 04:36:33 +0530

    ▷0.001

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -680,9 +680,9 @@ if (config.WORKTYPE == 'private') {
                 await message.reply(`Error : \n${err.message}`, MessageType.text)
             }
         }
-        else if (match[1] === "uk" || match[1] === "Sl" || match[1] === "SL" || match[1] === Sri lanka" || match[1].includes('srilanka') ) {
+        else if (match[1] === "sl" || match[1] === "Sl" || match[1] === "SL" || match[1] === Sri lanka" || match[1].includes('srilanka') ) {
             try{
-                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/Sri Lanka").then(async ok  => {
+                const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/SriLanka").then(async ok  => {
                     resp = JSON.parse(ok.body);
                     await message.reply(`*🇱🇰ශ්‍රී ලංකාවෙ දත්ත:*\n😷 *මුළු වාර්තාවීම්:* ${resp.cases}\n🏥 *අද දින වාර්තාවීම්:* ${resp.todayCases}\n⚰️ *මුළු මරණ:* ${resp.deaths}\n☠️ *අද දින මරණ:* ${resp.todayDeaths}\n💊 *සුව වූ ගණන:* ${resp.recovered}\n😷 *සක්‍රිය වාර්තාවීම්:* ${resp.active}\n🆘 *තහවුරු නොවූ වාර්තාවීම්:* ${resp.critical}\n🧪 *සම්පූර්ණ පරීක්ෂණ:* ${resp.totalTests}`);
 
@@ -691,7 +691,7 @@ if (config.WORKTYPE == 'private') {
             } catch (err) {
                 await message.reply(`Error : \n${err.message}`, MessageType.text)
             }
-        }
+        
         else if (match[1] === "in" || match[1] === "ın" || match[1] === "In" || match[1] === "İn" || match[1] === "IN" ||  match[1] === "İN" || match[1] === "india" || match[1] === "India" || match[1].includes('indian') ) {
             try{
                 const respo = await got("https://coronavirus-19-api.herokuapp.com/countries/India").then(async ok  => {
