83d132b4b562f99cb50efaa841ccb8cf5a9e713e
Teenuh-X <88734918+NeQHSqyJk@users.noreply.github.com>
2021-08-11 02:52:37 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -30,7 +30,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
         mesaj = '';
         grup['participants'].map(
             async (uye) => {
-                mesaj += '⚜ @' + uye.id.split('@')[0] + ' ';
+                mesaj += '⚜ @' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         );
@@ -42,7 +42,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
         mesaj = '';
         grup['participants'].map(
             async (uye) => {
-                mesaj += '⚜ @' + uye.id.split('@')[0] + ' ';
+                mesaj += '⚜ @' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         );
@@ -54,7 +54,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
         mesaj = '';
         grup['participants'].map(
             async (uye) => {
-                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                mesaj += '@' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         );
