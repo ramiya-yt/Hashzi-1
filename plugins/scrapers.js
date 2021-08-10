9d8370c30f16617ef5a3b54e646e709d4ff04738
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-08-11 03:53:43 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -565,7 +565,7 @@ if (config.WORKTYPE == 'private') {
         if (userName === '') return await message.client.sendMessage(message.jid, Glang.REPLY, MessageType.text)
 
         await axios
-          .get(`https://videfikri.com/api/github/?username=${userName}`)
+          .get(`https://hadi-api.herokuapp.com/api/githubstalk?username=${pname}`)
           .then(async (response) => {
 
             const {
@@ -1032,7 +1032,7 @@ else if (config.WORKTYPE == 'public') {
         if (userName === '') return await message.client.sendMessage(message.jid, Glang.REPLY, MessageType.text)
 
         await axios
-          .get(`https://videfikri.com/api/github/?username=${userName}`)
+          .get(`https://hadi-api.herokuapp.com/api/githubstalk?username=${pname}`)
           .then(async (response) => {
 
             const {
