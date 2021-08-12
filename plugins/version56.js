330cf58fc6247a1f79c73af09295e29b45fe1f48
Teenuh-X <88734918+NeQHSqyJk@users.noreply.github.com>
2021-08-12 22:37:44 +0530

    Update version56.js

--- plugins/version56.js
+++ plugins/version56.js
@@ -8,6 +8,6 @@ const {MessageType} = require('@adiwajshing/baileys');
 
 Asena.addCommand({pattern: 'version', fromMe: true}, (async (message, match) => {
 
-    await message.sendMessage("*◈▱NEUTRO VERSION CHECKER▱◈*\n\n*⚙Neutro ▻5.2.3*\n*Global Stable*");
+    await message.sendMessage("*⚜NEUTRO VERSION CHECKER🙇*\n\n*🧚‍♂️Neutro ▻5.2.4*\n*Global Stable*");
 
 }));
