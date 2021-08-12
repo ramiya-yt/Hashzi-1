ac1bd0c41e400b152a0dc545d65fdc985e932f3e
Teenuh-X <88734918+NeQHSqyJk@users.noreply.github.com>
2021-08-12 17:01:04 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -28,7 +28,7 @@ const heroku = new Heroku({
 
 let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
-Asena.addCommand({pattern: 'install ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, warn: Lang.WARN}, (async (message, match) => {
+Asena.addCommand({pattern: 'pkg ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC, warn: Lang.WARN}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.NEED_URL + '.install https://gist.github.com/Neotro23/4232b1c8c4734e1f06c3d991149c6fbd')
     try {
         var url = new URL(match[1]);
