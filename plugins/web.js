6bdd68d3f5af7755f6fdf7739b6dac44dff89ade
Teenuh-X <88734918+NeQHSqyJk@users.noreply.github.com>
2021-08-16 02:55:03 +0530

    url Shortner New Lock 🌜 .short

--- plugins/web.js
+++ plugins/web.js
@@ -4,6 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
+සන්සාරයේ මා හමු වී තෙබේදෝ...
 */
 
 const Asena = require('../events');
