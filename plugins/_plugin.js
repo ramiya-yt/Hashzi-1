072701e85bae88f3401094ac34b65c21115aad67
Tenuu122456 <89250551+Tenuu122456@users.noreply.github.com>
2021-08-23 04:24:02 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -2,6 +2,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 NEOTROX - TEENUHX
+*/
 
 const Asena = require('../events');
 const Heroku = require('heroku-client');
@@ -107,4 +108,4 @@ Asena.addCommand({pattern: 'remove(?: |$)(.*)', fromMe: true, desc: Lang.REMOVE_
     }
 
 }));
-*/
+
