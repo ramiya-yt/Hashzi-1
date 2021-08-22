1c73523d8a255b04883b51714925c22a545f818d
Tenuu122456 <89250551+Tenuu122456@users.noreply.github.com>
2021-08-22 14:15:05 +0530

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,9 +1,7 @@
-/* Copyright (C) 2021 whatsasena
-
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-usufusta
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
