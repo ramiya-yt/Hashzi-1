c114f270289b58b974b323ae7bb0d50a44c8f2b6
Tenuu122456 <89250551+Tenuu122456@users.noreply.github.com>
2021-08-25 18:40:46 +0530

    custom Wallpaper caption added

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -1912,7 +1912,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+        await message.client.sendMessage(message.jid, Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.BOT_NAME})
 
     }));
 }
